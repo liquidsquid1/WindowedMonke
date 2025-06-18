28cfa0070fed024119e42bf74ab7ed195a369039
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2025-06-18 21:08:35 +0100

    add mod files

---
+++ WindowedMonke.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace WindowedMonke {
+    [BepInPlugin("com.squid.windowedmonke", "WindowedMonke", "1.0.0")]
+    public class WindowedMonke : BaseUnityPlugin
+    {
+        void Start()
+        {
+            Screen.SetResolution(1280, 720, false);
+        }
+    }
+}
